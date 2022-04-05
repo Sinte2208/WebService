8a73dfa4d26157be3c56f939fb144b5f8e3118cc
sintesuandika <sinteagus@gmail.com>
2022-04-05 10:40:19 +0800

    second commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Tentang Saya</title>
+    </head>
+    <body>
+        <h2>Tentang Saya</h2>
+        <p>Nama lengkap saya Nyoman Agus Sinte dan sekarang saya berumur 22 tahun. Saya memiliki kondisi kesehatan yang sangat baik. Saya sangat hobby berolahraga dan memancing.</p>
+        <h2>Data Pribadi</h2>
+        <h4>Tempat lahir  : Mataram </h4>
+        <h4>Tanggal lahir : 22 Agustus 2000</h4>
+        <h4>Agama         : Hindu</h4>
+        <h4>Jenis Kelamin : Laki-Laki</h4>
+        <h4>Status        : Belum Menikah</h4>
+        <a href="{{ url('/')}}">Kembali</a>
+    </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>My Profile</title>
+    </head>
+    <body>
+        <div class="container"><center>
+        <img src="jcw.jpg" style="width: 100px; height:100px;">
+        <h1>Nyoman Agus Sinte Suandika</h1>
+        <h2>Teknik Informatika</h2>
+        <h5>Alamat : Jalan Danau Towutti No. 4-6 BTN Bumi Pagutan Permai</h5>
+        <h5>IG : @sintesuandika_</h5>
+        <h5>No Telefon : 082234456671</h5>
+        <h5>Email : sintepunya@gmail.com</h5>
+</center></div>
+               <h3><b>Pendidikan</b></h3>
+<ul>
+    <li>2007-2013 SD Negeri 7 Mataram</li>
+    <li>20013-2016 SMP Negeri 1 Mataram</li>
+    <li>2016-2019 SMA Negeri 2 Mataram</li>
+    <li>2019-sekarang Universitas Bumigora</li>
+</ul>
+<h3><b>Organisasi</b></h3>
+<ul>
+    <li>20010-2013 Pramuka</li>
+    <li>2013-2014 Pramuka</li>
+    <li>2013-2015 Osis SMP Negeri 1 Mataram</li>
+    <li>2016-2017 Organisasi Hindu SMA Negeri 2 Mataram</li>
+    <li>2016-2017 Pradah Kota Mataram</li>
+</ul>
+<a href="{{ route('tentang-saya')}}">Tentang Saya</a>
+           </body>
+</html>
